8e79b0b7db608d5bc019f4a586aa98b3d51c5bc2
Adonai Vera <adonai.vera@gmail.com>
2025-04-19 14:45:43 -0500

    restore to init the operator

--- __init__.py
+++ __init__.py
@@ -5,11 +5,10 @@ os.environ['FIFTYONE_ALLOW_LEGACY_ORCHESTRATORS'] = 'true'
 import fiftyone as fo
 import fiftyone.operators as foo
 from fiftyone.operators import types    
-
-# Import operators
 from .dataset_difficulty_operator import DatasetDifficultyScoring
 
 def register(plugin):
     """Register operators with the plugin."""
-    # Register the dataset difficulty scoring operator
-    foo.register_operator(DatasetDifficultyScoring)
+    # Register individual task operators
+    plugin.register(DatasetDifficultyScoring)
+    
